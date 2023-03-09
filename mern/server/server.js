7c9f2d0d115fbe0f2eb2cbbbe5577fb3ea483cfd
Srividhya2900 <telusrividhya2900@gmail.com>
2023-03-09 10:27:16 +0000

    added controller

--- mern/server/server.js
+++ mern/server/server.js
@@ -2,13 +2,16 @@ const express = require("express");
 require("dotenv").config(); //added
 
 const connectDB = require("./config/db"); //added
-
+const users = require("./routes/users"); // added
 const app = express();
+
 connectDB();//added
 
 // initialize middleware
 app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.send("Server up and running"));
+
+app.use("/api/users", users);
 
 // setting up port
 const PORT = 8080;
