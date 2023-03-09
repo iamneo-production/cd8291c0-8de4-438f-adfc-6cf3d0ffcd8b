fdeab15956f80193ecf0a8cf3cfe41c7de0044f6
Srividhya2900 <telusrividhya2900@gmail.com>
2023-03-09 08:39:25 +0000

    Mongodb connection

--- mern/server/server.js
+++ mern/server/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
+require("dotenv").config(); //added
+
+const connectDB = require("./config/db"); //added
 
 const app = express();
+connectDB();//added
 
 // initialize middleware
 app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.send("Server up and running"));
 
 // setting up port
-const PORT = 8081;
+const PORT = 8080;
 
 app.listen(PORT, () => {
     console.log(`server is running on http://localhost:${PORT}`);
