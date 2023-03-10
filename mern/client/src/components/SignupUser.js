520823856a946e1e9df79d85c9c6bbd24aff89e0
Srividhya2900 <telusrividhya2900@gmail.com>
2023-03-10 08:20:08 +0000

    signup

--- mern/client/src/components/SignupUser.js
+++ mern/client/src/components/SignupUser.js
@@ -5,20 +5,40 @@ import Col from 'react-bootstrap/Col'
 import {Link} from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
 export default function SignupUser() {
+  
+  const [data, setData] = useState({ firstname: "", lastname: "",email:"", password:"" });
+
+  function handleChange(e) {
+    setData((data) => ({ ...data, [e.target.name]: e.target.value }));
+}
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    axios
+        .post("https://8080-bfadcedcbbcaedacbacdcbdacdccdabcdebacdacedf.project.examly.io/api/user/register", data)
+        .then((res) => {
+            setData({ firstname: "", lastname: "",email:"",password:"" });
+            console.log(res.data.message);
+        })
+        .catch((err) => {
+            console.log("Error couldn't create TODO");
+            console.log(err.data.message);
+        });
+}
   return (
     <div>
-      <Form>
+      <Form onSubmit={handleSubmit}>
           <h1>Looking for a Job?</h1>
       <Row>
         <Col>
-          <Form.Control placeholder="First name" />
+          <Form.Control onChange={handleChange} placeholder="First name" />
         </Col>
         <Col>
-          <Form.Control placeholder="Last name" />
+          <Form.Control onChange={handleChange} placeholder="Last name" />
         </Col>
         <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control onChange={handleChange} type="email" placeholder="Enter email" />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -26,7 +46,7 @@ export default function SignupUser() {
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control onChange={handleChange} type="password" placeholder="Password" />
       </Form.Group>
       <Link to='/signupemployer'> <p>Sign up as an employer</p></Link>
       <Button variant="primary" type="submit" className="user-signup-submit">
